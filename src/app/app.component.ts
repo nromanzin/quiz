69405e43bb4a26ece00c6e26e89c00a166ab21a2
nromanzin <nicolas.romanzin@gmail.com>
2024-04-11 22:23:18 +0200

    1er jet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioChange, MatRadioModule } from '@angular/material/radio';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatCardModule,
+    MatRadioModule,
+    MatToolbarModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'quiz';
+  title = 'Quiz TUE/TFUE';
+
+  questions = [
+    {
+      prompt: 'Question 1',
+      options: ['Réponse A', 'Réponse B', 'Réponse C'],
+      solution: 'Réponse B',
+    },
+    {
+      prompt: 'Question 2',
+      options: ['Réponse A', 'Réponse B', 'Réponse C'],
+      solution: 'Réponse C',
+    },
+    {
+      prompt: 'Question 3',
+      options: ['Réponse A', 'Réponse B', 'Réponse C'],
+      solution: 'Réponse A',
+    },
+  ];
+
+  displayAnswer = false;
+
+  selectedAnswer = '';
+  answer = '';
+  selectAnswer(event: MatRadioChange) {
+    this.selectedAnswer = event.value;
+  }
+  validate() {
+    this.displayAnswer = true;
+  }
 }
